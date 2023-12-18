4245218b6d90ba1c6bbc99b410f561b16dfe6bc4
BasicAcid <david.tabarie@gmail.com>
2023-12-19 00:05:38 +0100

    Move graphviz code to specific file.

---
+++ graph.c
@@ -0,0 +1,51 @@
+#include </usr/include/graphviz/gvc.h>
+
+/* graphviz **************************************************************/
+
+/* Agraph_t *graph; */
+/* GVC_t *gvc; */
+
+/* gvc = gvContext(); */
+/* graph = agopen("my_graph", Agdirected, NULL); */
+
+/* Agnode_t *node1, *node2; */
+/* Agedge_t *edge; */
+
+/* // Add createflag argument to agnode calls */
+/* node1 = agnode(graph, "Node 1", 1); */
+/* node2 = agnode(graph, "Node 2", 1); */
+/* edge = agedge(graph, node1, node2, NULL, 1); */
+
+/* gvLayout(gvc, graph, "dot"); */
+/* gvRenderFilename(gvc, graph, "png", "output.png"); */
+
+/* gvFreeLayout(gvc, graph); */
+/* agclose(graph); */
+
+
+
+/* Agraph_t *graph; */
+/* GVC_t *gvc; */
+
+/* gvc = gvContext(); */
+/* graph = agopen("my_graph", Agdirected, NULL); */
+
+/* while (stack.top >= 0) { */
+/*     struct Value* node = pop(&stack); */
+/*     printf("Data: %f, Label: %s, Grad: %f\n", node->data, node->label, node->grad); */
+
+/* } */
+
+/* Agnode_t *node1, *node2; */
+/* //Agedge_t *edge; */
+
+/* // Add createflag argument to agnode calls */
+/* node1 = agnode(graph, "Node 1", 1); */
+/* node2 = agnode(graph, "Node 2", 1); */
+/* //edge = agedge(graph, node1, node2, NULL, 1); */
+
+/* gvLayout(gvc, graph, "dot"); */
+/* gvRenderFilename(gvc, graph, "png", "output.png"); */
+
+/* gvFreeLayout(gvc, graph); */
+/* agclose(graph); */
