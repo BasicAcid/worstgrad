4a7895a18a4ffc60eb772f4e000067ffcd251832
BasicAcid <david.tabarie@gmail.com>
2023-12-24 16:37:42 +0100

    Refactoring.

--- mlp.c
+++ mlp.c
@@ -2,27 +2,17 @@
 #include <stdlib.h>
 #include "main.h"
 
-/* struct MLP */
-/* *create_mlp(int nin, int n_out) */
-/* { */
-/*     size_t mlp_size = sizeof(struct MLP) + (long unsigned int)n_out * sizeof(struct Neuron); */
-/*     struct MLP *mlp = malloc(mlp_size); */
-
-/*     for(int i = 0; i < n_out; i++) */
-/*     { */
-/*     } */
-
-/*     return mlp; */
-/* } */
-
 struct MLP
 create_mlp(int nin, int n_layers)
 {
     struct MLP mlp;
     mlp.nin = nin;
 
-    mlp.nouts = (int *)malloc((long unsigned int)n_layers * sizeof(int));
-    mlp.layers = (struct Layer *)malloc((long unsigned int)n_layers * sizeof(struct Layer));
+    size_t nout_size = (long unsigned int)n_layers * sizeof(int);
+    size_t layer_size = (long unsigned int)n_layers * sizeof(struct Layer);
+
+    mlp.nouts = (int *)malloc(nout_size);
+    mlp.layers = (struct Layer *)malloc(layer_size);
 
     return mlp;
 }
